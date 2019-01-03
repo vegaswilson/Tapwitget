97c37d5ec4bc99a0d98e1d74675ff2ddf94114c8
vegaswilson <top_t.123@hotmail.com>
2019-01-03 12:04:58 +0700

    Initial commit

---
+++ app/src/main/java/com/easy/tapwitget/SongsAct.java
@@ -0,0 +1,4 @@
+package com.easy.tapwitget;
+
+public class SongsAct {
+}
---
+++ app/src/main/java/com/easy/tapwitget/tabwitget.java
@@ -0,0 +1,13 @@
+package com.easy.tapwitget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tabwitget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabwidget);
+    }
+}
